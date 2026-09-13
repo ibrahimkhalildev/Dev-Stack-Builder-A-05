9a7c9453ef4c6872317da41a5a3a5a5a5d767c99
ibrahimkhalildev <ibrahimkholil01@gmail.com>
2026-09-13 22:19:07 +0600

    A message 'Loading technologies...' is added by Suspense

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { Suspense } from 'react'
 import './App.css'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
@@ -9,7 +10,18 @@ function App () {
     <div>
       <Navbar></Navbar>
       <Hero></Hero>
-      <Technologies></Technologies>
+      <Suspense
+        fallback={
+          <div className='py-20 text-center'>
+            <div className='w-8 h-8 mx-auto border-4 border-gray-200 border-t-[#DB2777] rounded-full animate-spin'></div>
+            <p className='mt-2 text-sm text-gray-500'>
+              Loading technologies...
+            </p>
+          </div>
+        }
+      >
+        <Technologies></Technologies>
+      </Suspense>
       <Footer></Footer>
     </div>
   )
