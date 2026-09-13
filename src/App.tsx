d4dd3ebc75104ef63d9b503678d0e6c34ca76c1f
ibrahimkhalildev <ibrahimkholil01@gmail.com>
2026-09-13 15:59:41 +0600

    Project Setup Done

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
-
 import './App.css'
 
 function App () {
-
-
-  return <>
-  
-  
-  
-  </>
+  return (
+    <>
+      <h2 className='text-5xl text-red-500'>Assignment 05</h2>
+      <button className="btn btn-warning">Warning</button>
+    </>
+  )
 }
 
 export default App
